ae543c8cfb78bc3be5e91389a4893ed7a080dcd0
cryptoli <812143550@qq.com>
2024-08-25 01:40:30 +0800

    自动创建日志文件目录

--- utils/logging.py
+++ utils/logging.py
@@ -1,6 +1,10 @@
 import logging
+import os
 
 def setup_logger(name, log_file, level=logging.INFO):
+    # 确保日志文件所在的目录存在
+    os.makedirs(os.path.dirname(log_file), exist_ok=True)
+
     handler = logging.FileHandler(log_file)
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
     handler.setFormatter(formatter)

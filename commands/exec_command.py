398576aee22309b0043c781109ba516cb7ef99ba
cryptoli <812143550@qq.com>
2024-08-25 02:34:58 +0800

    修复执行命令bug

--- commands/exec_command.py
+++ commands/exec_command.py
@@ -1,9 +1,9 @@
 from telegram import Update
 from telegram.ext import CallbackContext
 from utils.logging import bot_logger
+from commands.execute_on_selected_servers import execute_on_selected_servers  # 导入执行命令的函数
 
-async def exec_command(update: Update, context:
-CallbackContext) -> None:
+async def exec_command(update: Update, context: CallbackContext) -> None:
     command = update.message.text.strip()
     context.user_data['command'] = command
 
@@ -14,3 +14,6 @@ CallbackContext) -> None:
 
     await update.message.reply_text(f"已选择的服务器: {', '.join(selected_servers)}\n正在执行命令: {command}")
     bot_logger.info(f"用户 {update.effective_user.id} 在服务器 {', '.join(selected_servers)} 上执行命令: {command}")
+
+    # 调用 execute_on_selected_servers 函数来执行命令
+    await execute_on_selected_servers(update, context)
